b9b8ddf79994673ff18e5fb82963ec7cdfb04d67
Malak Albattire <Malak.albattire@gmail.com>
2026-02-16 20:33:55 +0200

    add enums

---
+++ PharmacyManagement.Domain/Common/Enums/CreditStatus.cs
@@ -0,0 +1,8 @@
+namespace PharmacyManagement.Domain.Common.Enums;
+
+public enum CreditStatus
+{
+    Pending = 1,
+    Paid = 2,
+    Overdue = 3
+}
---
+++ PharmacyManagement.Domain/Common/Enums/TransactionType.cs
@@ -0,0 +1,7 @@
+namespace PharmacyManagement.Domain.Common.Enums;
+
+public enum TransactionType
+{
+    Credit = 1,
+    Debit = 2
+}
