7b4a7287b58584c6802faeedeb3dd3ced4056441
malakalbattire <98422017+malakalbattire@users.noreply.github.com>
2026-02-16 20:20:10 +0200

    Add JSON options for enum serialization
    Configure JSON options to use JsonStringEnumConverter.

--- PharmacyManagement.API/Program.cs
+++ PharmacyManagement.API/Program.cs
@@ -10,12 +10,25 @@ using PharmacyManagement.Infrastructure.Data;
 using PharmacyManagement.Infrastructure.Repositories;
 using PharmacyManagement.Infrastructure.Services;
 using System.Text;
+using System.Text.Json.Serialization;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllers();
+// builder.Services.AddControllers();
+// builder.Services.AddEndpointsApiExplorer();
+
+// Add services to the container
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
 builder.Services.AddEndpointsApiExplorer();
+
 
 // Swagger Configuration - FIXED
 builder.Services.AddSwaggerGen(options =>
