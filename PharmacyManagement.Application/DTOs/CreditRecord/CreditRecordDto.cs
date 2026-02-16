99563115fc4d7ed50e975d3c13c2f6758c0f5843
malakalbattire <98422017+malakalbattire@users.noreply.github.com>
2026-02-16 20:19:16 +0200

    Update CreditRecordDto.cs

--- PharmacyManagement.Application/DTOs/CreditRecord/CreditRecordDto.cs
+++ PharmacyManagement.Application/DTOs/CreditRecord/CreditRecordDto.cs
@@ -1,14 +1,17 @@
-﻿namespace PharmacyManagement.Application.DTOs.CreditRecord;
+using PharmacyManagement.Domain.Common.Enums;
+namespace PharmacyManagement.Application.DTOs.CreditRecord;
 
 public class CreditRecordDto
 {
+    public TransactionType Type { get; set; }
     public string CustomerName { get; set; } = string.Empty;
     public string CustomerPhone { get; set; } = string.Empty;
     public decimal TotalAmount { get; set; }
     public decimal PaidAmount { get; set; }
     public decimal RemainingAmount { get; set; }
     public DateTime DueDate { get; set; }
-    public string Status { get; set; } = "Pending";
+    // public string Status { get; set; } = "Pending";
+    public CreditStatus Status { get; set; } = CreditStatus.Pending;
     public string Notes { get; set; } = string.Empty;
 }
 
