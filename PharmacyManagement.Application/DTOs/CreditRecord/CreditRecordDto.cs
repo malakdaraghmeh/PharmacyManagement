6cd61c6ea7f1ebb7c3392cf7ce12eba8b4cfa0c9
Malak Albattire <Malak.albattire@gmail.com>
2026-02-18 21:21:57 +0200

    make notes and total is optional

--- PharmacyManagement.Application/DTOs/CreditRecord/CreditRecordDto.cs
+++ PharmacyManagement.Application/DTOs/CreditRecord/CreditRecordDto.cs
@@ -6,13 +6,13 @@ public class CreditRecordDto
     public TransactionType Type { get; set; }
     public string CustomerName { get; set; } = string.Empty;
     public string CustomerPhone { get; set; } = string.Empty;
-    public decimal TotalAmount { get; set; }
+    public decimal? TotalAmount { get; set; }
     public decimal PaidAmount { get; set; }
     // public decimal RemainingAmount { get; set; }
     public DateTime DueDate { get; set; }
     // public string Status { get; set; } = "Pending";
     public CreditStatus Status { get; set; } = CreditStatus.Pending;
-    public string Notes { get; set; } = string.Empty;
+    public string? Notes { get; set; } = string.Empty;
 }
 
 public class CreditRecordResponseDto : CreditRecordDto
